e994b79598ebd69b811dc86ec90e86d2105fb5fe
Dwinlight <corentinpouget15@gmail.com>
2019-10-07 08:46:44 +0200

    implementation non triee avec nouveaux tests pour la fusion

--- modularity/src/implementation/PQueueImplNonTrieeSimple.java
+++ modularity/src/implementation/PQueueImplNonTrieeSimple.java
@@ -15,7 +15,9 @@ public class PQueueImplNonTrieeSimple<T extends Comparable<T>> implements PQueue
     public PQueueImplNonTrieeSimple() {
         this.elements = new ArrayList<T>();
     }
-
+    public PQueueImplNonTrieeSimple(List<T> elements) {
+        this.elements = elements;
+    }
     @Override
     public void ajouter(T e) {
         this.elements.add(e);
@@ -38,16 +40,17 @@ public class PQueueImplNonTrieeSimple<T extends Comparable<T>> implements PQueue
 
     @Override
     public PQueue<T> fusion(PQueue<T> q) {
-        PQueue<T> fusionne = this;
+        PQueue<T> fusionne = new PQueueImplNonTrieeSimple<>(this.elements);
         while(!q.estVide()) {
             fusionne.ajouter(q.retirer().get());
         }
+        System.out.println(this.elements.size());
         return fusionne;
     }
 
     public static void main (String[] args){
         PQueueImplNonTrieeSimple<Tuple> file = new PQueueImplNonTrieeSimple<Tuple>();
-        Tuple t1 = new Tuple(1,"first");
+        Tuple t1 = new Tuple(1,"FIRST");
         Tuple t2 = new Tuple(2,"SECOND");
         Tuple t3 = new Tuple(3,"THIRD");
         Tuple t4 = new Tuple(6,"FOURTH");
@@ -75,6 +78,39 @@ public class PQueueImplNonTrieeSimple<T extends Comparable<T>> implements PQueue
         System.out.println(r5.equals(t2) ? "[Test 7]: OK": "[Test 7]: FAILED ----- Should get: " + t2.getElement() + " but get: " + r5.getElement());
         System.out.println(r6.equals(t1) ? "[Test 8]: OK": "[Test 8]: FAILED ----- Should get: " + t1.getElement() + " but get: " + r6.getElement());
         System.out.println(file.estVide() == true ? "[Test 9]: OK": "[Test 9]: FAILED");
+
+        PQueueImplNonTrieeSimple<Tuple> file2 = new PQueueImplNonTrieeSimple<Tuple>();
+        Tuple c1 = new Tuple(4,"ICHI");
+        Tuple c2 = new Tuple(8,"NI");
+        Tuple c3 = new Tuple(12,"SAN");
+        Tuple c4 = new Tuple(0,"YON");
+        Tuple c5 = new Tuple(7,"GO");
+        Tuple c6 = new Tuple(5,"ROKU");
+
+        ArrayList<Tuple> check = new ArrayList<Tuple>();
+        check.add(c1);
+        check.add(c2);
+        check.add(c3);
+        check.add(c4);
+        check.add(c5);
+        check.add(c6);
+        check.add(t1);
+        check.add(t2);
+        check.add(t3);
+        check.add(t4);
+        check.add(t5);
+        check.add(t6);
+        PQueue<Tuple> merge = file.fusion(file2);
+        System.out.println(file.elements.size() == 6 ?"[Test 10]: OK": "[Test 10]: FAILED");
+        int j = 0;
+        while(!merge.estVide() && check.contains(merge.retirer())){
+            j++;
+        }
+
+        System.out.println(j == 12 && merge.estVide() ?"[Test 11]: OK": "[Test 11]: FAILED  ---- length= "+j+" and empty = "+ merge.estVide());
+
+
+
 
 
     }
